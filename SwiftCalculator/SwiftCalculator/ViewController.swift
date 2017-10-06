aa6db9b859c72aae9468ba6030b395029703cfd1
lxt <lxt@lxtdeMacBook-Air.local>
2017-10-06 11:47:42 +0800

    初始化项目

---
+++ SwiftCalculator/SwiftCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCalculator
+//
+//  Created by lxt on 2017/10/6.
+//  Copyright © 2017年 com.blueeye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SwiftCalculator/SwiftCalculatorTests/SwiftCalculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftCalculatorTests.swift
+//  SwiftCalculatorTests
+//
+//  Created by lxt on 2017/10/6.
+//  Copyright © 2017年 com.blueeye. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftCalculator
+
+class SwiftCalculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
